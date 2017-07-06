bba64f47ae4f01fb0504752c733c99c8e4a5cd4d
Aliaksie <a.darafeyeu@gmail.com>
2017-07-06 11:34:42 +0300

    Progress with cabinet and dashboard...

--- app/js/controllers/registration.js
+++ app/js/controllers/registration.js
@@ -1,8 +1,119 @@
 'use strict';
 
 angular.module('iConnectApp')
-    .controller('RegistrationController', ['$scope', '$state', 'loginFactory', function ($scope, $state, loginFactory) {
+    .controller('RegistrationController', ['$scope', '$state', 'loginFactory', 'artistFactory', function ($scope, $state, loginFactory, artistFactory) {
         $scope.Person = {};
+        $scope.artistList = [
+            {
+                id: 0,
+                firstName: "Nicole",
+                lastName: "Pearson",
+                country: "Belarus",
+                style: "Signer",
+                discrabe: "Read, out with friends, listen to music, draw and learn new things.",
+                photo: "http://placehold.it/100x100"
+            },
+            {
+                id: 1,
+                firstName: "Nicole",
+                lastName: "Pearson",
+                country: "Belarus",
+                style: "Signer",
+                discrabe: "Read, out with friends, listen to music, draw and learn new things.",
+                photo: "http://placehold.it/100x100"
+            }];
+        $scope.organizationList = [
+            {
+                id: 0,
+                title: "Sura Records",
+                location: "Belarus",
+                discrabe: "Read, out with friends, listen to music, draw and learn new things.",
+                photo: "http://placehold.it/100x100"
+            },
+            {
+                id: 1,
+                title: "Pary Rai",
+                location: "Belarus",
+                discrabe: "Read, out with friends, listen to music, draw and learn new things.",
+                photo: "http://placehold.it/100x100"
+            }
+        ];
+        $scope.eventsArt = [{
+            id: 0,
+            title: 'Romantic Ballads',
+            imgUrl: 'http://placehold.it/150x200',
+            eventText: 'Read, out with friends, listen to music, draw and learn new things.',
+            eventType: 'Concert',
+            eventLocation: 'UK, London',
+            eventDate: '12 nov 2016, 22:00 - 5:00',
+            eventArtist: [
+                {
+                    id: 1,
+                    artistImg: 'http://placehold.it/60x60'
+                },
+                {
+                    id: 1,
+                    artistImg: 'http://placehold.it/60x60'
+                },
+                {
+                    id: 0,
+                    artistImg: 'images/Add_small@2x.png'
+                },
+                {
+                    id: 0,
+                    artistImg: 'images/Add_small@2x.png'
+                },
+                {
+                    id: 0,
+                    artistImg: 'images/Add_small@2x.png'
+                }
+            ]
+
+        },
+            {
+                id: 0,
+                title: 'Romantic Ballads',
+                imgUrl: 'http://placehold.it/150x200',
+                eventText: 'Read, out with friends, listen to music, draw and learn new things.',
+                eventType: 'Concert',
+                eventLocation: 'UK, London',
+                eventDate: '12 nov 2016, 22:00 - 5:00',
+                eventArtist: [
+                    {
+                        id: 1,
+                        artistImg: 'http://placehold.it/60x60'
+                    },
+                    {
+                        id: 1,
+                        artistImg: 'http://placehold.it/60x60'
+                    },
+                    {
+                        id: 1,
+                        artistImg: 'http://placehold.it/60x60'
+                    },
+                    {
+                        id: 1,
+                        artistImg: 'http://placehold.it/60x60'
+                    },
+                    {
+                        id: 0,
+                        artistImg: 'images/Add_small@2x.png'
+                    },
+                    {
+                        id: 0,
+                        artistImg: 'images/Add_small@2x.png'
+                    },
+                    {
+                        id: 0,
+                        artistImg: 'images/Add_small@2x.png'
+                    },
+                    {
+                        id: 0,
+                        artistImg: 'images/Add_small@2x.png'
+                    }
+                ]
+
+            }];
 
         function gotoPersonalPage() {
             if ($scope.Person.type === 'artist') {
