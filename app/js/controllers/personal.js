1b6d08669511236b39b9eb225ff7c5ae05734b6c
Aliaksie <a.darafeyeu@gmail.com>
2017-07-11 21:43:09 +0300

    update style...

--- app/js/controllers/personal.js
+++ app/js/controllers/personal.js
@@ -104,6 +104,50 @@ angular.module('iConnectApp')
                     }
                 ]
 
+            },
+            {
+                id: 0,
+                title: 'Romantic Ballads',
+                imgUrl: 'http://placehold.it/150x200',
+                eventText: 'Read, out with friends, listen to music, draw and learn new things.',
+                eventType: 'Concert',
+                eventLocation: 'UK, London',
+                eventDate: '12 nov 2016, 22:00 - 5:00',
+                eventArtist: [
+                    {
+                        id: 1,
+                        artistImg: 'http://placehold.it/60x60'
+                    },
+                    {
+                        id: 1,
+                        artistImg: 'http://placehold.it/60x60'
+                    },
+                    {
+                        id: 1,
+                        artistImg: 'http://placehold.it/60x60'
+                    },
+                    {
+                        id: 1,
+                        artistImg: 'http://placehold.it/60x60'
+                    },
+                    {
+                        id: 0,
+                        artistImg: 'images/Add_small@2x.png'
+                    },
+                    {
+                        id: 0,
+                        artistImg: 'images/Add_small@2x.png'
+                    },
+                    {
+                        id: 0,
+                        artistImg: 'images/Add_small@2x.png'
+                    },
+                    {
+                        id: 0,
+                        artistImg: 'images/Add_small@2x.png'
+                    }
+                ]
+
             }
             ]
 
