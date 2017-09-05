0708f02b850e67a5505853948c77d5d4b34cd6bd
Dima Serikov <dserikov7@gmail.com>
2017-09-05 16:20:45 +0300

    Big Calendar

--- app/js/controllers/organization.js
+++ app/js/controllers/organization.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('iConnectApp')
-    .controller('OrganizationController', ['$scope' , 'organizationFactory', 'artistFactory', function($scope, organizationFactory, artistFactory) {
+    .controller('OrganizationController', ['$scope' , 'organizationFactory', 'artistFactory', 'ngDialog',  function($scope, organizationFactory, artistFactory, ngDialog) {
         $scope.organizationList = organizationFactory.getOrganizationList();
         $scope.newArtists = artistFactory.getNewArtists();
 
@@ -20,5 +20,27 @@ angular.module('iConnectApp')
 
         $scope.locations = organizationFactory.getLocationList();
         $scope.types = organizationFactory.getTypesList();
+
+        $scope.showCalendarEvents = function (e) {
+            e.preventDefault();
+
+            $('.col').removeClass('selected');
+            $(e.target).closest('.col').addClass('selected');
+
+            console.log('Shows in right sidebar events.');
+        };
+
+        $scope.setBusiness = function (e) {
+            e.preventDefault();
+
+            if ($(e.target).hasClass('col')) {
+                ngDialog.open({
+                    template: $(e.target).hasClass('locked') ? '../../views/template/dialog-business-free.html' : '../../views/template/dialog-business-busy.html',
+                    className: 'ngdialog-theme-default',
+                    plain: false
+                    //controller: 'SomeController'
+                });
+            }
+        };
 
     }]);
