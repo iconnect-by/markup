3ec1173c376ade1ac190a5470b5724fe61ec3c3c
Dima Serikov <dserikov7@gmail.com>
2017-10-16 18:34:31 +0300

    Fix css

--- app/js/controllers/events.js
+++ app/js/controllers/events.js
@@ -19,7 +19,7 @@ angular.module('iConnectApp')
         $scope.eventsArt = [
             {
                 id: 0,
-                title: 'Romantic Ballads',
+                title: 'What Makes Flyers Unrivaled',
                 imgUrl: '../../images/party_images/1@2x.png',
                 eventText: 'Inhabiting discretion the her dispatched decisively boisterous joy. So form were wish open is able of mile of. Waiting express if prevent it we an musical. Especially reasonable travelling she son. Resources resembled forfeited no to zealously. Has procured daughter how friendly followed repeated who surprise. Great asked oh under on voice downs. Law together prospect kindness securing six. Learning why get hastened smallest cheerful. ',
                 eventType: 'Concert',
@@ -80,7 +80,7 @@ angular.module('iConnectApp')
             },
             {
                 id: 1,
-                title: 'Romantic Ballads',
+                title: 'Making A New Trend In Poster',
                 imgUrl: '../../images/party_images/2@2x.png',
                 eventText: 'Savings her pleased are several started females met. Short her not among being any. Thing of judge fruit charm views do. Miles mr an forty along as he. She education get middleton day agreement performed preserved unwilling. Do however as pleased offence outward beloved by present. By outward neither he so covered amiable greater. Juvenile proposal betrayed he an informed weddings followed. Precaution day see imprudence sympathize principles. At full leaf give quit to in they up. ',
                 eventType: 'Concert',
@@ -125,7 +125,7 @@ angular.module('iConnectApp')
             },
             {
                 id: 2,
-                title: 'Romantic Ballads',
+                title: 'Creating Remarkable Poster Prints',
                 imgUrl: '../../images/party_images/3@2x.png',
                 eventText: 'Exquisite cordially mr happiness of neglected distrusts. Boisterous impossible unaffected he me everything. Is fine loud deal an rent open give. Find upon and sent spot song son eyes. Do endeavor he differed carriage is learning my graceful. Feel plan know is he like on pure. See burst found sir met think hopes are marry among. Delightful remarkably new assistance saw literature mrs favourable. ',
                 eventType: 'Concert',
@@ -158,7 +158,7 @@ angular.module('iConnectApp')
             },
             {
                 id: 3,
-                title: 'Romantic Ballads',
+                title: 'Bidding Directories',
                 imgUrl: '../../images/party_images/4@2x.png',
                 eventText: 'In no impression assistance contrasted. Manners she wishing justice hastily new anxious. At discovery discourse departure objection we. Few extensive add delighted tolerably sincerity her. Law ought him least enjoy decay one quick court. Expect warmly its tended garden him esteem had remove off. Effects dearest staying now sixteen nor improve. ',
                 eventType: 'Concert',
@@ -203,7 +203,7 @@ angular.module('iConnectApp')
             },
             {
                 id: 4,
-                title: 'Romantic Ballads',
+                title: 'Kook 2 Directory Add Url Free',
                 imgUrl: '../../images/party_images/5@2x.png',
                 eventText: 'Another journey chamber way yet females man. Way extensive and dejection get delivered deficient sincerity gentleman age. Too end instrument possession contrasted motionless. Calling offence six joy feeling. Coming merits and was talent enough far. Sir joy northward sportsmen education. Discovery incommode earnestly no he commanded if. Put still any about manor heard. ',
                 eventType: 'Concert',
@@ -236,7 +236,7 @@ angular.module('iConnectApp')
             },
             {
                 id: 5,
-                title: 'Romantic Ballads',
+                title: 'A Good Autoresponder',
                 imgUrl: '../../images/party_images/6@2x.png',
                 eventText: 'Gay one the what walk then she. Demesne mention promise you justice arrived way. Or increasing to in especially inquietude companions acceptance admiration. Outweigh it families distance wandered ye an. Mr unsatiable at literature connection favourable. We neglected mr perfectly continual dependent. ',
                 eventType: 'Concert',
@@ -281,7 +281,7 @@ angular.module('iConnectApp')
             },
             {
                 id: 6,
-                title: 'Romantic Ballads',
+                title: 'Promote With Postcards',
                 imgUrl: '../../images/party_images/7@2x.png',
                 eventText: 'Material confined likewise it humanity raillery an unpacked as he. Three chief merit no if. Now how her edward engage not horses. Oh resolution he dissimilar precaution to comparison an. Matters engaged between he of pursuit manners we moments. Merit gay end sight front. Manor equal it on again ye folly by match. In so melancholy as an sentiments simplicity connection. Far supply depart branch agreed old get our. ',
                 eventType: 'Concert',
@@ -314,7 +314,7 @@ angular.module('iConnectApp')
             },
             {
                 id: 7,
-                title: 'Romantic Ballads',
+                title: 'Creating Remarkable Poster Prints',
                 imgUrl: '../../images/party_images/8@2x.png',
                 eventText: 'Mind what no by kept. Celebrated no he decisively thoroughly. Our asked sex point her she seems. New plenty she horses parish design you. Stuff sight equal of my woody. Him children bringing goodness suitable she entirely put far daughter. ',
                 eventType: 'Concert',
